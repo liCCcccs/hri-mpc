3f720652e820437398f3c827806e85cefbf5af4f
MingruiSun2019 <sunmr2017@gmail.com>
2024-02-15 15:32:51 +1100

    angle measurement implemented

--- mpc/util.py
+++ mpc/util.py
@@ -4,6 +4,7 @@ from torch.nn import Module
 from torch.nn.parameter import Parameter
 
 import operator
+import numpy as np
 
 
 def jacobian(f, x, eps):
@@ -166,3 +167,21 @@ def data_maybe(x):
     if x is None:
         return None
     return x.data
+
+
+def fftnoise(f):
+    f = np.array(f, dtype="complex")
+    Np = (len(f) - 1) // 2
+    phases = np.random.rand(Np) * 2 * np.pi
+    phases = np.cos(phases) + 1j * np.sin(phases)
+    f[1 : Np + 1] *= phases
+    f[-1 : -1 - Np : -1] = np.conj(f[1 : Np + 1])
+    return np.fft.ifft(f).real
+
+
+def band_limited_noise(min_freq, max_freq, samples=1024, samplerate=1, noise_level=1):
+    freqs = np.abs(np.fft.fftfreq(samples, 1 / samplerate))
+    f = np.zeros(samples)
+    idx = np.where(np.logical_and(freqs >= min_freq, freqs <= max_freq))[0]
+    f[idx] = noise_level
+    return fftnoise(f)
